7a9f003c7ef4409f7a772b23cf279a27c090e9be
kingwilldabeast <willmur@live.com>
2024-07-17 10:31:49 -0500

    able to view events specific to a venue

--- Tick-It FrontEnd/src/components/VenueExpandedPage.jsx
+++ Tick-It FrontEnd/src/components/VenueExpandedPage.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
-import {useParams, Link} from 'react-router-dom'
+import {useParams, Link, useNavigate} from 'react-router-dom'
 import axios from 'axios'
 
 
 export default function VenueExpandedPage (props) {
 
   const [venue, setVenue] = useState('')
+  const [events, setEvents] = useState([])
 
   let {venueID} = useParams() 
+
+  let navigate = useNavigate()
+  const showevent = (eventID) => {
+    navigate(`/events/${eventID}`)
+  }
 
   useEffect(() => {
     const getData = async () => {
@@ -16,6 +22,12 @@ export default function VenueExpandedPage (props) {
       
       //assign API results to array
       setVenue(responseVenue.data)
+
+      const responseEvents = await axios.get(`http://127.0.0.1:8000/events`)
+      console.log('data', responseEvents)
+      
+      //assign API results to array
+      setEvents(responseEvents.data)
     }
     getData()
 
@@ -35,6 +47,31 @@ export default function VenueExpandedPage (props) {
         className='expandedImage'
         />
       </div>
+
+      <div className="objectList">
+      <p className='header'>Events for this venue</p>
+      {
+        events.map((event, index) => (
+          event.venue_id == venueID ?
+            <div key={index} 
+            className="objectItem"
+            onClick={() => showevent(event.id)}
+            style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${event.image_url})`, 
+                backgroundSize:'600px', 
+                backgroundPosition: 'center 30%'}} 
+            >
+                
+                <h2>{event.name}</h2>
+                <h3>Price: {event.price}</h3>
+            </div>    
+        :
+        null
+        ))
+      }
+
+    </div>
+
     </div>
   ) 
   :     <div> 
