38192a1253ff255fdaddb532a09b761e923f76d2
kingwilldabeast <willmur@live.com>
2024-07-17 09:24:32 -0500

    added the delete function

--- Tick-It FrontEnd/src/components/EventExpandedPage.jsx
+++ Tick-It FrontEnd/src/components/EventExpandedPage.jsx
@@ -6,11 +6,31 @@ import axios from 'axios'
 export default function EventExpandedPage (props) {
 
   const [event, setEvent] = useState('')
+  let {eventID} = useParams() 
   let navigate = useNavigate()
+
   const openEditor = (e) => {
     navigate(`/editevent/${eventID}`);
   }
-  let {eventID} = useParams() 
+
+const deleteEvent = async (e) => {
+    try {
+        const response = await axios.delete(`http://127.0.0.1:8000/events/${eventID}`, {
+            headers: {
+            "Content-Type": "application/json",
+            },
+        });
+            console.log(response.status)
+            if (response.status === 200 || response.status === 204) {
+                console.log("event deleted");
+                navigate(`/events`);
+            } else {
+                console.error("Failed to delete event:", response.statusText);
+            }
+        } catch (error) {
+        console.error("Error:", error)
+        }
+}
 
   useEffect(() => {
     const getData = async () => {
@@ -33,6 +53,7 @@ export default function EventExpandedPage (props) {
         <h2>{event.name}</h2>
         <h3>Details: {event.details}</h3>
         <button onClick={openEditor}>Edit Event</button>
+        <button onClick={deleteEvent}>Delete Event</button>
         </div>
         <img src = {event.image_url} 
         className='expandedImage'
